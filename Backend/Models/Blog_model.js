b72a134e5f532e9f9457c4c1a2e0f9b931e307e5
Nayem Hossain <nayemhossssain@gmail.com>
2023-07-25 00:26:08 +0600

    database model modified

--- Backend/Models/Blog_model.js
+++ Backend/Models/Blog_model.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
+  user:{
+    type:mongoose.Schema.Types.ObjectId,
+    required:true,
+  },
   title: {
     type: String,
     required: true,
@@ -21,6 +25,31 @@ const blogSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  like:{
+    type:Array,
+  },
+  dislike:{
+    type:Array,
+  },
+  comments:[
+    {
+              user:{
+                        type:mongoose.Schema.ObjectId,
+                        required:true
+              },
+              username:{
+                        type:String,
+                        required:true
+              },
+              profile:{
+                        type:String
+              },
+              comment:{
+                        type:String,
+                        required:true
+              }
+    }
+  ]
 });
 
 const blogs = mongoose.model('blogs', blogSchema);
