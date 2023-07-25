5159aff9946651ff0a90410fdcacd8216b967293
Rashedul Islam <rashedul.rajon@gmail.com>
2023-07-25 22:15:53 +0600

    user Controller updated

--- Backend/Controller/User_controller.js
+++ Backend/Controller/User_controller.js
@@ -2,6 +2,8 @@ const createError = require('http-errors')
 const userModel=require('../Models/User_model');
 const { hashPassword, comparePassword } = require('../helper/auth');
 const jwt =require("jsonwebtoken");
+const OTPSModel = require("../Models/OTP_model");
+const SendEmailUtility = require("../utility/SendEmailUtility");
 require("dotenv").config();
 
 
@@ -9,7 +11,7 @@ require("dotenv").config();
 exports.registerController=async(req,res)=>{
     try {
        // 1. destructure name, email, password from req.body
-       const{firstName,lastName,phoneNumber,email,password}=req.body;
+       const{firstName,lastName,username,phoneNumber,email,password}=req.body;
        // 2. all fields require validation
        if(!firstName.trim()){
           return res.send({message:"firststName is required"})
@@ -17,6 +19,9 @@ exports.registerController=async(req,res)=>{
        if(!lastName.trim()){
           return res.send({message:"lastName is required"})
        }
+        if(!username.trim()){
+            return res.send({message:"lastName is required"})
+        }
        if(!email){
           return res.send({message:"email is required"})
        }
@@ -42,7 +47,7 @@ exports.registerController=async(req,res)=>{
        const hashedPassword=await hashPassword(password)
        // save
        const user=await new userModel({
-        firstName,lastName,email,phoneNumber,password:hashedPassword
+        firstName,lastName,username,email,phoneNumber,password:hashedPassword
        }).save();
        res.status(201).send({
           success:true,
@@ -115,59 +120,120 @@ exports.loginController = async(req,res)=>{
     } 
  }
 
-// forgot password controller
 
-exports.forgotPasswordController=async(req,res)=>{
+ //ProfileUpdate
+exports.ProfileUpdate=async (req, res) => {
     try {
-       const{email,answer,newPassword}=req.body;
-       if(!email){
-          res.status(400).send({message:"Email is required"})
-       }
-       if(!answer){
-          res.status(400).send({message:"answer is required"})
-       }
-       if(!newPassword){
-          res.status(400).send({message:"New Password is required"})
-       }
- 
-       // check
-       const user=await userModel.findOne({email,answer})
-       if(!user){
-          return res.status(404).send({
-             success:false,
-             message:"wrong email or answer"
-          })
-       }
-       const hashed=await hashPassword(newPassword)
-       await userModel.findByIdAndUpdate(user._id,{password:hashed});
-       res.status(200).send({
-          success:true,
-          message:"Password reset success"
-       })
+        let data = await userModel.updateOne({email: req.headers['email']}, req.body)
+        return {status: "success", data: data}
     } catch (error) {
-       console.log(error);
-       res.status(500).send({
-          success:false,
-          message:"something went wrong",
-          error
-       })
+        return {status: "fail", data: error.toString()}
     }
- }
+}
 
- //test controller
-exports.testController = (req, res) => {
-    try {
-      res.send("Protected Routes");
-    } catch (error) {
-      console.log(error);
-     // res.send({ error });
-    }
-  };
 
-exports.getUser=(req,res,next)=>{
+
+//ProfileDetails
+exports.ProfileDetails=async (req, res) => {
     try {
-        res.status(200).send({message:"user already"});
+        let data = await userModel.findOne({ email: req.headers['email'] });
+        return { status: "success", data: data };
     } catch (error) {
-        next(error);
+        return { status: "fail", data: error.toString() };
     }
+}
+
+
+//varify Email
+exports.RecoverVerifyEmail=async (req,res)=>{
+
+    try {
+        // Email Account Query
+        let email = req.params.email;
+        let OTPCode = Math.floor(100000 + Math.random() * 900000)
+
+        // Database First process
+        let UserCount = (await userModel.aggregate([{$match: {email: email}}, {$count: "total"}]))
+
+        if(UserCount.length>0){
+            // OTP Insert
+
+            // Database Second process
+            await OTPSModel.create({email: email, otp: OTPCode})
+
+            // Email Send
+            let SendEmail = await SendEmailUtility(email,"Your PIN Code is= "+OTPCode,"Inventory PIN Verification")
+
+            return {status: "success", data: SendEmail}
+        }
+        else{
+            return {status: "fail", data: "No User Found"}
+        }
+    }catch (error) {
+
+        return {status: "fail", data: error.toString()}
+    }
+
+}
+
+
+//verifyOTP
+exports.RecoverVerifyOTP=async (req,res)=>{
+
+    try {
+        let email = req.params.email;
+        let OTPCode = req.params.otp;
+        let status=0;
+        let statusUpdate=1;
+
+
+        //Database First Process
+        let OTPCount = await userModel.aggregate([{$match: {email: email, otp: OTPCode, status: status}}, {$count: "total"}])
+
+        if (OTPCount.length>0) {
+
+            // Second Process
+            let OTPUpdate = await userModel.updateOne({email: email, otp: OTPCode, status: status}, {email: email, otp: OTPCode, status: statusUpdate})
+            return {status: "success", data: OTPUpdate}
+
+        } else {
+
+            return  {status: "fail", data: "Invalid OTP Code"}
+        }
+    }
+    catch (error) {
+        return {status: "fail", data: error.toString()}
+    }
+
+}
+
+
+//resetPass
+exports.RecoverResetPass=async (req,res)=>{
+
+    let email = req.body['email'];
+    let OTPCode = req.body['OTP'];
+    let NewPass =  req.body['password'];
+    let statusUpdate=1;
+
+    try {
+        // Database First Process
+        let OTPUsedCount = await OTPSModel.aggregate([{$match: {email: email, otp: OTPCode, status: statusUpdate}}, {$count: "total"}])
+
+        if (OTPUsedCount.length>0) {
+            // Database Second Process
+            let PassUpdate = await userModel.updateOne({email: email},{password: NewPass})
+            return {status: "success", data: PassUpdate}
+        }
+
+        else {
+            return {status: "fail", data: "Invalid Request"}
+        }
+    }
+
+
+    catch (e) {
+        return {status: "fail", data: e.toString()}
+    }
+
 }
