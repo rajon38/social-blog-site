4a56c12682bb93eb0f43fc9ecd1684edfa611004
Rashedul Islam <rashedul.rajon@gmail.com>
2023-07-25 22:37:01 +0600

    update

--- Backend/secret.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-
-const ServerPort = process.env.SERVER_PORT||6000
-const mongoDB=process.env.MongoDB_URL || "mongodb://localhost:27017/blogwebsite"
-
-module.exports = {ServerPort,mongoDB}
